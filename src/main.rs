282a7fbee9e16ca27dcb3f8d196f071e9a4d1eef
jan <not-wlan@protonmail.com>
2019-10-27 21:35:17 +0100

    Add graceful HTTP 500 handler

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ extern crate serde_derive;
 extern crate diesel;
 #[macro_use]
 extern crate diesel_derive_enum;
-use rocket::{get, http::Status, routes, Request, catch, catchers, post};
+use rocket::{catch, catchers, get, http::Status, post, routes, Request};
 use rocket_contrib::json::Json;
 
 use serde::Serialize;
@@ -16,8 +16,7 @@ mod database;
 mod schema;
 mod sql_types;
 
-use crate::database::Connection;
-use crate::sql_types::Signature;
+use crate::{database::Connection, sql_types::Signature};
 
 #[derive(Serialize)]
 struct ServerStatus {
@@ -33,6 +32,15 @@ struct TenebraeError {
     errors: Vec<String>,
 }
 
+impl TenebraeError {
+    pub fn new(message: &str) -> Self {
+        TenebraeError {
+            version: env!("CARGO_PKG_VERSION").to_string(),
+            errors: vec![message.to_string()],
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize)]
 struct TenebraeSearch {
     signatures: Vec<String>,
@@ -41,14 +49,14 @@ struct TenebraeSearch {
 #[derive(Serialize, Deserialize)]
 struct TenebraeResult {
     name: String,
-    signature: String
+    signature: String,
 }
 
 impl From<&sql_types::Signature> for TenebraeResult {
     fn from(sig: &Signature) -> Self {
-        TenebraeResult{
+        TenebraeResult {
             name: sig.name.clone(),
-            signature: sig.signature.clone()
+            signature: sig.signature.clone(),
         }
     }
 }
@@ -58,25 +66,33 @@ struct TenebraeSearchResult {
     signatures: Vec<TenebraeResult>,
 }
 
+#[catch(500)]
+fn service_unavailable(_: &Request) -> Json<TenebraeError> {
+    Json(TenebraeError::new("Service currently unavailable!"))
+}
+
 #[catch(404)]
 fn not_found(_: &Request) -> Json<TenebraeError> {
-    Json(TenebraeError{
-        version: env!("CARGO_PKG_VERSION").to_string(),
-        errors: vec!["The requested resource was not found!".to_string()]
-    })
+    Json(TenebraeError::new("The requested resource was not found!"))
 }
 
 #[post("/search", data = "<query>")]
-fn search(query: Json<TenebraeSearch>, connection: Connection) -> Result<Json<TenebraeSearchResult>, Status> {
-    let result = sql_types::Signature::search(&query.signatures, &connection).map_err(|_| Status::ServiceUnavailable)?;
-    Ok(Json(TenebraeSearchResult{
-        signatures: result.iter().map(TenebraeResult::from).collect()
+fn search(
+    query: Json<TenebraeSearch>,
+    connection: Connection,
+) -> Result<Json<TenebraeSearchResult>, Status> {
+    let result = sql_types::Signature::search(&query.signatures, &connection)
+        .map_err(|_| Status::ServiceUnavailable)?;
+    Ok(Json(TenebraeSearchResult {
+        signatures: result.iter().map(TenebraeResult::from).collect(),
     }))
 }
 
 #[get("/signature/<id>")]
 fn fetch(id: i32, connection: Connection) -> Result<Json<sql_types::Signature>, Status> {
-    sql_types::Signature::fetch(id, &connection).map_err(|_| Status::NotFound).map(Json)
+    sql_types::Signature::fetch(id, &connection)
+        .map_err(|_| Status::NotFound)
+        .map(Json)
 }
 
 #[get("/")]
@@ -94,7 +110,7 @@ fn index(connection: Connection) -> Result<Json<ServerStatus>, Status> {
 fn main() {
     rocket::ignite()
         .manage(database::connect())
-        .register(catchers![not_found])
+        .register(catchers![not_found, service_unavailable])
         .mount("/", routes![index, fetch, search])
         .launch();
 }
